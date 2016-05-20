043c0387c666110a955da4b8e08b6ac64400d88e
Andrei <apollo-3@mail.ru>
2016-05-20 12:40:59 +0000

    changed user limits

--- public/providers/values.js
+++ public/providers/values.js
@@ -30,7 +30,7 @@ app.factory('values', function() {
         projects: 2,
         flats: 10,
         photos: 4,
-        logs: 1
+        logs: 2
       },
       advanced: {
         projects: 5,
